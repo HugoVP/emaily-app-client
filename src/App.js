047adac7f49e8dbc29d23edee0c630d3a2a05aea
HugoVP <dev@paleo.host>
2018-11-16 12:07:20 -0600

    Various updates
    - Adding sign in option
    - Adding .dockerignore file
    - Updating yarn.lock

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ class App extends Component {
           >
             Learn React
           </a>
+          <a className="App-link" href="/auth/google">
+            Sign In With Google
+          </a>
         </header>
       </div>
     );
